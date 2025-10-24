2ea2d86ef5e32efa88e768c45b04070cb6bae737
parsa jalilian <parsajlylyan032@gmail.com>
2025-10-24 16:27:29 +0200

    a

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+    // id("dev.flutter.flutter-gradle-plugin") // <<<<<<<<<< این خط برای رفع خطا حذف شد
 }
 
 // <<<<<<< 2. این بلوک با سینتکس Kotlin بازنویسی شده است >>>>>>>>>
